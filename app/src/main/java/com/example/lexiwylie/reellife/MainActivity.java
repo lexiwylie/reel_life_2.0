c2445bb8ee55846f66d12c2989ee80509289dc3e
lexiwylie <lexiwylie13@gmail.com>
2020-11-09 21:25:56 -0600

    button functionality not working

--- app/src/main/java/com/example/lexiwylie/reellife/MainActivity.java
+++ app/src/main/java/com/example/lexiwylie/reellife/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.lexiwylie.reellife;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private TextView reelLife;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }

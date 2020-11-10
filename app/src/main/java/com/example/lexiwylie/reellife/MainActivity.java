6846a639be0cec90d731be1aa3b12ff42be803fc
lexiwylie <lexiwylie13@gmail.com>
2020-11-09 21:30:29 -0600

    Revert "button functionality not working"
    This reverts commit c2445bb8ee55846f66d12c2989ee80509289dc3e.

--- app/src/main/java/com/example/lexiwylie/reellife/MainActivity.java
+++ app/src/main/java/com/example/lexiwylie/reellife/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.lexiwylie.reellife;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Toast;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
-{
-    private TextView reelLife;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
